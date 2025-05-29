1879fcf8a0c4deffa0b46e8f9fbe2d1ce2fec8a5
OutFRid <xkxindong@gmail.com>
2025-05-29 18:02:35 +0800

    update Day3

---
+++ Day3/test_file_upload.py
@@ -0,0 +1,20 @@
+import os
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+def test_uploads():
+    driver = webdriver.Chrome()
+    driver.get("https://the-internet.herokuapp.com/upload")
+    upload_file = os.path.abspath(
+        os.path.join(os.getcwd(), "..", "Day3", "fileToBeUpload.png"))
+
+    file_input = driver.find_element(By.CSS_SELECTOR, "input[type='file']")
+    file_input.send_keys(upload_file)
+    driver.find_element(By.ID, "file-submit").click()
+
+    file_name_element = driver.find_element(By.ID, "uploaded-files")
+    file_name = file_name_element.text
+
+    assert file_name == "fileToBeUpload.png"
